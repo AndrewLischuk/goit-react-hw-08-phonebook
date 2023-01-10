8db74153600934193e8e193938c0b448671dc279
Andrew Lischuk <andrewlischuk59@gmail.com>
2023-01-10 09:18:00 +0200

    fix issue

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,9 @@
 import { Appbar } from 'components/Appbar/Appbar';
 import { Outlet } from 'react-router-dom';
-import styles from './Layout.module.css';
 
 export const Layout = () => {
   return (
-    <div className={styles.layout}>
+    <div>
       <Appbar />
       <Outlet />
     </div>
